788d980ba031d0d4c42ef453a45fff307bed5894
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-10-25 14:27:39 -0200

    sctp: validate chunk len before actually using it
    Andrey Konovalov reported that KASAN detected that SCTP was using a slab
    beyond the boundaries. It was caused because when handling out of the
    blue packets in function sctp_sf_ootb() it was checking the chunk len
    only after already processing the first chunk, validating only for the
    2nd and subsequent ones.
    
    The fix is to just move the check upwards so it's also validated for the
    1st chunk.
    
    Backport reference:
     * WORD_ROUND was later replaced with SCTP_PAD4
    
    Change-Id: I1e1c96e088f77231b319b728c8e4c6a6b541c6c0
    Reported-by: Andrey Konovalov <andreyknvl@google.com>
    Tested-by: Andrey Konovalov <andreyknvl@google.com>
    Signed-off-by: Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>
    Reviewed-by: Xin Long <lucien.xin@gmail.com>
    Acked-by: Neil Horman <nhorman@tuxdriver.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

---
+++ build.sh
@@ -0,0 +1,89 @@
+export USE_CCACHE=1
+export CCACHE_DIR=$PWD/.ccache
+if [ ! -d $PWD/.ccache ]; then
+ccache -M 1G
+fi
+export ARCH=arm64
+
+function get_make_command()
+{
+  echo command make
+}
+function make()
+{
+    local start_time=$(date +"%s")
+    $(get_make_command) "$@"
+    local ret=$?
+    local end_time=$(date +"%s")
+    local tdiff=$(($end_time-$start_time))
+    local hours=$(($tdiff / 3600 ))
+    local mins=$((($tdiff % 3600) / 60))
+    local secs=$(($tdiff % 60))
+    local ncolors=$(tput colors 2>/dev/null)
+    if [ -n "$ncolors" ] && [ $ncolors -ge 8 ]; then
+        color_failed=$'\E'"[0;31m"
+        color_success=$'\E'"[0;32m"
+        color_reset=$'\E'"[00m"
+    else
+        color_failed=""
+        color_success=""
+        color_reset=""
+    fi
+    echo
+    if [ $ret -eq 0 ] ; then
+        echo -n "${color_success}#### make completed successfully "
+    else
+        echo -n "${color_failed}#### make failed to build some targets "
+    fi
+    if [ $hours -gt 0 ] ; then
+        printf "(%02g:%02g:%02g (hh:mm:ss))" $hours $mins $secs
+    elif [ $mins -gt 0 ] ; then
+        printf "(%02g:%02g (mm:ss))" $mins $secs
+    elif [ $secs -gt 0 ] ; then
+        printf "(%s seconds)" $secs
+    fi
+    echo " ####${color_reset}"
+    echo
+    return $ret
+}
+
+
+if [ -d ~/toolchain/aarch64-4.9/bin/ ]; then
+	echo "Found toolchain: Setting up . . . "
+else
+	echo "Missing toolchain: Obtaining from Google . . ."
+	git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b master ~/toolchain/aarch64-4.9 
+fi
+export CROSS_COMPILE=~/toolchain/aarch64-4.9/bin/aarch64-linux-android-
+
+make Z00L_defconfig
+
+while read -p "Start build with:`echo $'\n ' `1) $(nproc) cores?`echo $'\n ' `2) $(($(nproc)/2)) cores?`echo $'\n '`3) $(($(nproc)/4)) cores?`echo $'\n '`X|x to cancel auto build`echo $'\n>'`" corechoice
+do
+case "$corechoice" in
+	1 )
+		echo "Building with $(nproc) cores"
+		make -j$(nproc)
+		break
+		;;
+
+        2 )
+		echo "Building with $(($(nproc)/2)) cores"
+		make -j$(($(nproc)/2))
+                break
+                ;;
+        3 )
+		echo "Building with $(($(nproc)/4)) core(s)"
+		make -j$(($(nproc)/4))
+		break
+                ;;
+        X|x )
+                echo "Cancelling auto build"
+                break
+                ;;
+        * )
+                echo "Invalid: Try again"
+                ;;
+
+esac
+done
